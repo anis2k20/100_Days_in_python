171b2ff20fdba689452b3258e2390b200ef05b44
Anis <anis2k20@gmail.com>
2023-09-25 09:39:21 +0600

    Spotify playlist

--- spotify_playlist/main.py
+++ spotify_playlist/main.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
+import requests
+import datetime
 
-with open("index.html") as file:
-    html_doc=file.read()
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+#date = datetime.date.today()
+date = "2023-09-23"
+response = requests.get("https://www.billboard.com/charts/hot-100/"+date)
 
-soup = BeautifulSoup(html_doc,'html.parser')
-print(soup.prettify())
+# with open("index.html") as file:
+#     html_doc=file.read()
+#
+soup = BeautifulSoup(response.text,'html.parser')
+song_name = soup.select("li ul li h3")
+song = [song.getText().strip() for song in song_name]
+print(song)
